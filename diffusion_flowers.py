af5cdb1fb07a6325675788cf1d2e6018979c4a00
nnolte <niklas.nolte@cern.ch>
2023-02-21 12:12:13 -0500

    updatei

--- diffusion_flowers.py
+++ diffusion_flowers.py
@@ -2,38 +2,38 @@
 import torch
 import torchvision
 import matplotlib.pyplot as plt
+from torch.nn import functional as F
 from torchvision.datasets import Flowers102
 from tqdm import tqdm
 
-torch.set_float32_matmul_precision('high')
+torch.set_float32_matmul_precision("high")
 # %%
 # HYPERPARAMS
-T = 100
+T = 50
 img_size = 64
 batch_size = 64
 embedding_dims = 32
 
 # reload the data with new transforms?
-RELOAD = False
+RELOAD = True
 
 
 # %%
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 # %%
 
-transforms = torchvision.transforms.Compose(
-    [
-        torchvision.transforms.RandomResizedCrop(img_size),
-        torchvision.transforms.ToTensor(),
-    ]
-)
+transforms = torch.nn.Sequential(
+  torchvision.transforms.RandAugment(2, 5),
+  torchvision.transforms.RandomResizedCrop(img_size),
+  torchvision.transforms.RandomHorizontalFlip(),
+).to(device)
 
 # %%
 train_flowers = Flowers102(
-    root="/data/ml_data", download=True, transform=transforms, split="train"
+    root="/data/ml_data", download=True, transform=torchvision.transforms.ToTensor(), split="train"
 )
 test_flowers = Flowers102(
-    root="/data/ml_data", download=True, transform=transforms, split="test"
+    root="/data/ml_data", download=True, transform=torchvision.transforms.ToTensor(), split="test"
 )
 # val_flowers = Flowers102(
 #     root="data/ml_data", download=True, transform=transforms, split="val"
@@ -45,7 +45,7 @@ if RELOAD:
     X += [f[0] for f in test_flowers]
     Y = [f[1] for f in train_flowers]
     Y += [f[1] for f in test_flowers]
-    X = torch.stack(X).to(device)
+    X = transforms(torch.stack(X).to(device))
     Y = torch.tensor(Y).to(device)
     # normalize X
     X = (X - X.mean()) / X.std()
@@ -55,13 +55,10 @@ else:
     X = torch.load("/data/ml_data/flowers-102/X.pt")
     Y = torch.load("/data/ml_data/flowers-102/Y.pt")
 
-
 # %%
 def sinusoidal_embedding(x, emb_dims, max_freq):
     frequencies = torch.exp(
-        torch.linspace(
-            0, torch.math.log(max_freq), emb_dims // 2
-            , device=x.device)
+        torch.linspace(0, torch.math.log(max_freq), emb_dims // 2, device=x.device)
     )
     angular_speeds = 2.0 * torch.math.pi * frequencies
     embeddings = torch.concat(
@@ -118,18 +115,19 @@ class decoder_block(torch.nn.Module):
 
 
 class UNet(torch.nn.Module):
-    def __init__(self, img_size, sample_steps):
+    def __init__(self):
         super().__init__()
-        self.e1 = encoder_block(4, 64)
+        self.e1 = encoder_block(embedding_dims + 3, 64)
         self.e2 = encoder_block(64, 128)
         self.b = conv_block(128, 256)
         self.d1 = decoder_block(256, 128)
         self.d2 = decoder_block(128, 64)
         self.outputs = torch.nn.Conv2d(64, 3, kernel_size=1, padding=0)
 
-    def forward(self, inputs, t):
-        enc = sinusoidal_embedding(t, embedding_dims, 1e3).view(-1, embedding_dims, 1, 1)
-
+    def forward(self, inputs, noise_level):
+        enc = sinusoidal_embedding(noise_level, embedding_dims, 1e3)
+        enc = enc.view(-1, embedding_dims, 1, 1)
+        enc = enc.repeat(1, 1, img_size, img_size)
         s1, p1 = self.e1(torch.cat([inputs, enc], axis=1))
         s2, p2 = self.e2(p1)
         b = self.b(p2)
@@ -144,39 +142,47 @@ class DenoisingDiffusionModel(torch.nn.Module):
     def __init__(self, T, minval, maxval):
         super().__init__()
         self.T = T  # maximum diffusion time
-        self.register_buffer("alpha", torch.linspace(maxval, minval, T))
-        self.register_buffer("alphaprod", torch.cumprod(self.alpha, dim=0))
+        startangle = torch.acos(torch.tensor(maxval, dtype=torch.float32))
+        endangle = torch.acos(torch.tensor(minval, dtype=torch.float32))
+        steps = torch.linspace(startangle, endangle, T)
+        noise_level = torch.sin(steps)
+        signal_level = torch.cos(steps)
+        self.register_buffer("noise_level", noise_level.view(-1, 1, 1, 1))
+        self.register_buffer("signal_level", signal_level.view(-1, 1, 1, 1))
 
     def diffusion_loss(self, model, X):
         # we sample from a N(0, 1)
         eps = torch.randn_like(X, device=X.device)
         # sample a time
         t = torch.randint(0, self.T, (X.shape[0],), device=X.device)
-        alpha = self.alphaprod[t].view(-1, 1, 1, 1)
-        # noise up X
-        signal_level = torch.sqrt(alpha)
-        noise_level = torch.sqrt(1 - alpha)
-        X_noisy = signal_level * X + noise_level * eps
+        X_noisy = self.signal_level[t] * X + self.noise_level[t] * eps
         # model is supposed to predict noise as close as possible to eps
-        eps_pred = model(X_noisy, noise_level)
-        return torch.nn.functional.mse_loss(eps_pred, eps)
+        eps_pred = model(X_noisy, self.noise_level[t]**2)
+        return F.l1_loss(eps_pred, eps)
 
-    def sample(self, model: torch.nn.Module, n_samples: int, dims: tuple, X0 = None):
+    def sample(self, model: torch.nn.Module, n_samples: int, dims: tuple, X0=None):
         with torch.no_grad():
             device = next(model.parameters()).device
             if X0 is None:
-              X = torch.randn(n_samples, *dims, device=device)
+                img_noisy = torch.randn(n_samples, *dims, device=device)
             else:
-              X = X0
+                img_noisy = X0
             for t in reversed(range(self.T)):
-                alphaprod = self.alphaprod[t]
-                alpha = self.alpha[t]
-                noise_level = torch.sqrt(1 - alpha)
-                eps_theta = model(X, torch.tensor([noise_level] * n_samples, device=device).view(-1, 1, 1, 1))
-                X_mean = X - (1 - alpha) / torch.sqrt(1 - alphaprod) * eps_theta
-                X_mean /= torch.sqrt(alpha)
-                X = X_mean + torch.randn_like(X_mean) * torch.sqrt(1 - alpha)
-            return X_mean  # that way we drop the last noise
+                signal_level, noise_level = self.signal_level[t], self.noise_level[t]
+                eps_pred = model(
+                    img_noisy,
+                    (noise_level ** 2).repeat(n_samples, 1, 1, 1),
+                )
+                img_pred = (img_noisy - noise_level * eps_pred) / signal_level
+
+                # noise it again for next iteration
+                next_signal_level, next_noise_level = (
+                    self.signal_level[t - 1],
+                    self.noise_level[t - 1],
+                )
+                eps_new = torch.randn_like(img_pred, device=device)
+                img_noisy = next_signal_level * img_pred + next_noise_level * eps_new
+            return img_pred  # that way we drop the last noise
 
 
 # %%
@@ -200,10 +206,10 @@ def train(model, optimizer, diffusion_model, X, n_epochs=50):
 
 
 # %%
-model = UNet(sample_steps=T, img_size=img_size).to(device)
-#model = torch.compile(model)
+model = UNet().to(device)
+# model = torch.compile(model)
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-4)
-diffusion_model = DenoisingDiffusionModel(T, 0.05, 0.95).to(device)
+diffusion_model = DenoisingDiffusionModel(T, .01, 0.95).to(device)
 # n params in model
 sum(p.numel() for p in model.parameters() if p.requires_grad)
 
@@ -217,17 +223,9 @@ output = diffusion_model.sample(model, 5, dims=X.shape[1:])
 output = output - output.min()
 output /= output.max()
 # %%
-#plot all
+# plot all
 fig, axs = plt.subplots(1, 5, figsize=(20, 4))
 for i in range(5):
     axs[i].imshow(output[i].permute(1, 2, 0).cpu().numpy())
 
-# %%
-output = diffusion_model.sample(model, 5, dims=X.shape[1:], X0=output[4])
-output = output - output.min()
-output /= output.max()
-# %%
-fig, axs = plt.subplots(1, 5, figsize=(20, 4))
-for i in range(5):
-    axs[i].imshow(output[i].permute(1, 2, 0).cpu().numpy())
 # %%
